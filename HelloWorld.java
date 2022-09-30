71b3fcd839b572b5d999df8657f7443fc2d0edb1
sathi1998 <113890479+sathi1998@users.noreply.github.com>
2022-09-30 17:27:33 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,6 +35,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    system.out.println("hello, good morning");
+    system.out.println("Hello, good morning");
   }
 }
